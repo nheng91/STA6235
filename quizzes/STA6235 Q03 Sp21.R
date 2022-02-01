dc3e6bbb975c3aa4200763ac8bf49ecd683fe430
Samantha Seals <sseals@uwf.edu>
2022-02-01 08:58:09 -0600

    add Q03

---
+++ quizzes/STA6235 Q03 Sp21.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+y <- c(46.4, 48.2, 46.5, 46.4, 48.6)
+x <- c(221, 221, 217, 216, 230)
+
+one <- tibble(x, y)
+
+m1 <- lm(y ~ x, data = one)
+
+summary(m1)
+
+# find residuals
+residuals(m1)
+
+# find predicted values
+predict(m1)
