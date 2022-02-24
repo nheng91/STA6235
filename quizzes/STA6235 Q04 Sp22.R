16c98dd0ca107ca707c2a9e9713c152fd0518c4a
Samantha Seals <sseals@uwf.edu>
2022-02-24 13:08:46 -0600

    add quizzes

---
+++ quizzes/STA6235 Q04 Sp22.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+y <- c(46.4, 48.2, 46.5, 46.4, 48.6)
+x <- c(221, 221, 217, 216, 230)
+
+one <- tibble(x, y)
+
+m1 <- lm(y ~ x, data = one)
+
+summary(m1)
+
+# find MSE
+anova(m1)
+
+# find CI
+confint(m1)
---
+++ quizzes/STA6235 Q05 Sp22.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+y <- c(46.4, 48.2, 46.5, 46.4, 48.6)
+x <- c(221, 221, 217, 216, 230)
+
+one <- tibble(x, y)
+
+m1 <- lm(y ~ x, data = one)
+
+summary(m1)
